574dc456c6ddff2ec23a7deafca9207729ef4d33
dannybreck <142409809+dannybreck@users.noreply.github.com>
2025-06-29 12:47:28 -0400

    Fix Metro configuration for TypeScript file extension error

--- metro.config.js
+++ metro.config.js
@@ -9,6 +9,10 @@ config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 // Ensure Metro correctly resolves node_modules
 config.resolver.nodeModulesPaths = [path.resolve(__dirname, 'node_modules')];
 
+// Enable package exports to handle modern package.json exports
+config.resolver.unstable_enablePackageExports = true;
+config.resolver.unstable_conditionNames = ['require', 'import', 'react-native', 'browser', 'default'];
+
 // Add TypeScript extensions to resolver
 config.resolver.sourceExts.push('ts', 'tsx');
 
