ee7b10d0b1079d11131ce3d455e0d3e5fcdf755b
dannybreck <142409809+dannybreck@users.noreply.github.com>
2025-06-29 12:45:15 -0400

    Fix Metro TypeScript configuration

--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,9 @@ const config = getDefaultConfig(__dirname);
 // Add support for web
 config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 
+// Add TypeScript extensions to resolver
+config.resolver.sourceExts.push('ts', 'tsx');
+
 // Add support for async storage
 config.resolver.alias = {
   '@react-native-async-storage/async-storage': require.resolve('@react-native-async-storage/async-storage'),
